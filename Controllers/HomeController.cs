e23b4559ec7c2ff648ae2d294d1dff4083965e4f
AmirAbbas Ranjbar <amir_abbas_r@yahoo.com>
2020-11-26 14:34:07 +0330

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public CrudApi.Models.CrudAPIEntities DBEntities { get; set; }
+        public HomeController()
+        {
+            DBEntities = new Models.CrudAPIEntities();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult SelectAll()
+        {
+            var model = DBEntities.USERS.Select(a => a).ToList();
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult Select(int id)
+        {
+            var model = DBEntities.USERS.Select(a => a.ID==id).SingleOrDefault();
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
